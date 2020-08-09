feb0b0d79e6f2f94050d293a8f203109f4c3324c
Miriam Mnyuku <mmnyuku@seattleu.edu>
2020-08-09 15:19:52 -0700

    Added login with facebook part 1

--- app/src/main/java/com/rent_a_friend/MainActivity.java
+++ app/src/main/java/com/rent_a_friend/MainActivity.java
@@ -3,7 +3,13 @@ package com.rent_a_friend;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
@@ -13,13 +19,53 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Arrays;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private LoginButton loginButton;
+    private CircleImageView circleImageView;
+    private TextView textName, textEmail;
+    private static final String EMAIL = "email";
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login_button);
+        textName = findViewById(R.id.profile_name);
+        textEmail = findViewById(R.id.profile_email);
+        //circleImageView = findViewById(R.id.profile_image);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        //loginButton.setFragment(this);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
