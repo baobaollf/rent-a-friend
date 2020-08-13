8a7ce87f0415be4ee65c878fe3a7255ae74832ba
baobaollf <55776365+baobaollf@users.noreply.github.com>
2020-08-13 14:58:48 -0500

    fetching data from firestorn and display them

--- app/src/main/java/com/rent_a_friend/ui/feed/FeedFragment.java
+++ app/src/main/java/com/rent_a_friend/ui/feed/FeedFragment.java
@@ -1,6 +1,7 @@
 package com.rent_a_friend.ui.feed;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,28 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.rent_a_friend.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import kotlin.collections.ArrayDeque;
+
 public class FeedFragment extends Fragment {
+
+    List<Map<String, Object>> list = new ArrayDeque<>();
+    List<String> username = new ArrayList<>();
+    List<String> imageUrl = new ArrayList<>();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Nullable
     @Override
@@ -22,8 +42,34 @@ public class FeedFragment extends Fragment {
         RecyclerView recyclerView = listItemsView.findViewById(R.id.feed_recyclerView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
+
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(), username, imageUrl);
+
         recyclerView.setAdapter(adapter);
+        getFeeds(adapter);
         return listItemsView;
+    }
+
+    private void getFeeds(final RecyclerViewAdapter adapter) {
+        db.collection("feeds").orderBy("username", Query.Direction.ASCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.d("data_base", "listen:error");
+                    return;
+                }
+                for (QueryDocumentSnapshot doc : value) {
+                    if(doc.get("image") != null) {
+                        Log.d("data_base", doc.getString("image"));
+                        imageUrl.add(doc.getString("image"));
+                    }
+                    if (doc.get("username") != null) {
+                        username.add(doc.getString("username"));
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/rent_a_friend/ui/feed/RecyclerViewAdapter.java
+++ app/src/main/java/com/rent_a_friend/ui/feed/RecyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.rent_a_friend.ui.feed;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,35 +12,35 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.rent_a_friend.R;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import kotlin.collections.ArrayDeque;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    List<String> bitmaps;
+    List<String> username;
+    Context context;
+    RecyclerViewAdapter(Context context, List<String> username, List<String> bitmaps) {
+        this.bitmaps = bitmaps;
+        this.username = username;
+        this.context = context;
+    }
+
+
     @NonNull
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // Access a Cloud Firestore instance from your Activity
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        final DocumentReference docRef = db.collection("feeds").document("32CSoWQkBI10Lhw0xNlb");
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d("data_base", String.valueOf(document.getData()));
-                    } else {
-                        Log.d("data_base", "no such document");
-                    }
-                } else {
-                    Log.d("data_base", "get failed with ", task.getException());
-                }
-            }
-        });
+
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.feed_card, parent, false);
         return new ViewHolder(view);
@@ -47,26 +49,29 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+        //Log.d("data_base", bitmaps.size() + " size");
+        //holder.username.setText((String)list.get(position).get("username"));
+        // Log.d("in_adapter", (String) Objects.requireNonNull(list.get(1).get("username")));
+        //holder.mainImage.setImageBitmap();
+        // Access a Cloud Firestore instance from your Activity
 
+        holder.username.setText(username.get(position));
+        Glide.with(context).load(bitmaps.get(position)).into(holder.mainImage);
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+        return username.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView mainImage;
         TextView username;
-        ImageView userIcon;
-        ImageView chatIcon;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mainImage = itemView.findViewById(R.id.feed_card_main_image);
             username = itemView.findViewById(R.id.feed_card_user_name);
-            userIcon = itemView.findViewById(R.id.feed_card_user_icon);
-            chatIcon = itemView.findViewById(R.id.feed_card_chat_icon);
         }
     }
 }
