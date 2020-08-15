62a20c2402a59432ff270735811f3c55f440d736
Miriam Mnyuku <mmnyuku@seattleu.edu>
2020-08-15 06:14:21 -0700

    Merge branch 'merge' of C:\Rent-a-friend\rent-a-friend with conflicts.

--- app/src/main/java/com/rent_a_friend/ui/home/HomeFragment.java
+++ app/src/main/java/com/rent_a_friend/ui/home/HomeFragment.java
@@ -29,6 +29,8 @@ public class HomeFragment extends Fragment
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+       // inflate the layout for this fragment
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
