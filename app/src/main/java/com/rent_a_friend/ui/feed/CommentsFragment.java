db67aeeb91bc6eb7fa5d0634042c1a4c181d4278
baobaollf <55776365+baobaollf@users.noreply.github.com>
2020-08-29 23:35:04 -0500

    comment function working

--- app/src/main/java/com/rent_a_friend/ui/feed/CommentsFragment.java
+++ app/src/main/java/com/rent_a_friend/ui/feed/CommentsFragment.java
@@ -1,10 +1,12 @@
 package com.rent_a_friend.ui.feed;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,26 +14,27 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.rent_a_friend.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+
 
 public class CommentsFragment extends Fragment {
     List<String> username = new ArrayList<>();
     List<String> comments = new ArrayList<>();
     String documentID;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    Button uploadCommentButton;
+    EditText editText;
     CommentsFragment(String documentID) {
         this.documentID = documentID;
     }
@@ -40,6 +43,8 @@ public class CommentsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View listItemsView = inflater.inflate(R.layout.fragment_comment, container, false);
+        uploadCommentButton = (Button) listItemsView.findViewById(R.id.upload_comment_button);
+        editText = (EditText) listItemsView.findViewById(R.id.comment_edit_text);
         RecyclerView recyclerView = listItemsView.findViewById(R.id.comment_recyclerView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
@@ -49,7 +54,28 @@ public class CommentsFragment extends Fragment {
 
         recyclerView.setAdapter(adapter);
         getFeeds(adapter);
+        uploadCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadComment();
+            }
+        });
         return listItemsView;
+    }
+
+    private void uploadComment() {
+        String text = editText.getText().toString();
+        if (text.length() >= 1) {
+            String path = "feeds" + "/" + documentID + "/" + "comments";
+            Map<String, Object> comment = new HashMap<>();
+            comment.put("comment", text);
+            comment.put("time", Calendar.getInstance().getTime());
+            comment.put("username", "comment upload user");
+            db.collection(path).add(comment);
+        } else {
+            Toast.makeText(getContext(), "Enter text to upload", Toast.LENGTH_SHORT).show();
+        }
+        editText.getText().clear();
     }
 
 
